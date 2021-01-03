c1137ddecb8d50dc6419e4af1b862f8f01a65e31
sedx876 <swatkins876@gmail.com>
2021-01-03 14:58:05 -0500

    added config and model folders

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from  'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -21,4 +25,6 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server running on port blahblahblah'))
+app.listen(5000, 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.rainbow.bold)
+)
