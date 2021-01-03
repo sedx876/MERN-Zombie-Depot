ab6cb8304640324839d1530b5adf426815fffe0d
sedx876 <swatkins876@gmail.com>
2021-01-03 11:40:20 -0500

    added nodemon concurrently dotenv dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -15,5 +18,7 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find(p => p._id === req.params.id)
   res.json(product)
 })
+
+const PORT = process.env.PORT || 5000
 
 app.listen(5000, console.log('Server running on port blahblahblah'))
