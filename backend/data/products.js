2ede7ff2b6afc1b5715ec557fbb29aeaf6350581
sedx876 <swatkins876@gmail.com>
2021-01-03 17:30:25 -0500

    added seeder script and seeder data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: '1',
     name: 'Kids Safety Cage',
     image: '/images/barbcage.jpg',
     description:
@@ -13,7 +12,6 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '2',
     name: 'Chainsaw Katana',
     image: '/images/chainsawkatana.jpg',
     description:
@@ -26,7 +24,6 @@ const products = [
     numReviews: 8,
   },
   {
-    _id: '3',
     name: 'Kids Starter Saw',
     image: '/images/kidsaw.jpg',
     description:
@@ -39,7 +36,6 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '4',
     name: 'Nokia Hammer',
     image: '/images/nokiahammer.jpg',
     description:
@@ -52,7 +48,6 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '5',
     name: 'BadA** Stroller',
     image: '/images/stroller.jpg',
     description:
@@ -65,7 +60,6 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: '6',
     name: 'Zombie Repellant Soap',
     image: '/images/zomsoap.jpg',
     description:
