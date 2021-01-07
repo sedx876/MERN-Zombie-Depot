e9e703e5cb398f7e4f8ba69db784c08b95cdd137
sedx876 <swatkins876@gmail.com>
2021-01-07 12:36:56 -0500

    added frontend dependencies and store.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import './index.css'
 import './bootstrap.min.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, 
+  document.getElementById('root')
+)
 
 
-reportWebVitals(console.log);
+reportWebVitals(console.log)
