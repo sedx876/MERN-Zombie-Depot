517853b2631d330c9dc533cb60b8d80e94fb20e5
sedx876 <swatkins876@gmail.com>
2021-01-08 08:20:22 -0500

    added message.js and loader.js

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -23,7 +23,7 @@ import {
   PRODUCT_TOP_REQUEST,
   PRODUCT_TOP_SUCCESS,
   PRODUCT_TOP_FAIL,
-} from '../constants/productConstants'
+} from '../constants/productConstants.js'
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
