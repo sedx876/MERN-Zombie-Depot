96f2b4d8dfc2bbad65f22d144a08499afba6899d
sedx876 <swatkins876@gmail.com>
2021-01-10 09:00:51 -0500

    added login.js and register.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <Route exact path='/' component={HomeScreen}/>
         <Route path='/product/:id' component={ProductScreen}/>
         <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/login' component={LoginScreen} />
+        <Route path='/register' component={RegisterScreen} />
         </Container>
       </main>
       <Footer/>
